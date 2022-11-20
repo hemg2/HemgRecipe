9139482c4099a4dc4fb1b751985ac388ad3df8c4
hemg2 <hemg2@naver.com>
2022-11-20 12:56:50 +0900

    Initial Commit

---
+++ hemgRecipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hemgRecipe
+//
+//  Created by 1 on 2022/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
