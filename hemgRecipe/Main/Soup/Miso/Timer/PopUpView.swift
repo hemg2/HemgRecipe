073394433541be68ba7ccb393e60aef8dcd59214
hemg2 <hemg2@naver.com>
2022-12-25 18:55:08 +0900

    Timer popUP Ui

---
+++ hemgRecipe/Main/Soup/Miso/Timer/PopUpView.swift
@@ -0,0 +1,22 @@
+//
+//  PopUpView.swift
+//  hemgRecipe
+//
+//  Created by 1 on 2022/12/25.
+//
+
+import UIKit
+
+
+class PopUpView: UIViewController {
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
